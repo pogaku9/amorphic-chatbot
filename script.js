a845424bfde15405be8edf3ba9a3d0fb572e6d29
Susheel Pogaku <susheel.pogaku@cloudwick.com>
2024-03-12 14:14:53 +0530

    update script

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 // PASTE the exported code here
+window.amorphicAIChatConfig = {
+    portalSource: "https://genai-sandbox.cwdl.cloudwick.com/embedded-chatbot/2c860812-43c2-4136-8be4-9a11fae0a968"
+};
